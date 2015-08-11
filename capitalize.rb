d0fb67dd41dc8612a0d03b9bd286b50f56189382
Peter Muturi <pmuturi65@gmail.com>
2015-08-11 19:41:32 +0300

    capiralize

--- capitalize.rb
+++ capitalize.rb
@@ -1,7 +1,5 @@
-	def LetterCapitalize ()
-    puts "enter a sentence"
-    sentence=gets.chomp
-    words =sentence.split(' ')
+def capitalize_words(string)
+    words =string.split(' ')
     
     words.each_with_index do |word, index|
         letters = word.split('')
@@ -9,13 +7,11 @@
         letters[0] = letters[0].upcase
         word = letters.join('')
         words[index] = word
-         #puts word
+        
     end
 
-    sentence = words.join(' ')
-    return sentence
+    string = words.join(' ')
+    return string
     
-    end
-  
-puts LetterCapitalize()
-  
+   
+end
